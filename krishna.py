1b179a1d307d3f1bc8437e441b72e10188b773de
krish <sanganakrishna743@gmail.com>
2021-11-15 20:24:12 +0530

    * TRIANGLE

--- krishna.py
+++ krishna.py
@@ -1,1 +1,8 @@
-print('hello world')
+for i in range(1,5):
+    for j in range(1,5+i-1):
+        if i+j<=4:
+            print(' ',end='')
+        else:
+            print('*',end='')
+
+    print()
